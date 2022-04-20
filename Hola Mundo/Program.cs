012c77d681ec79e833489f2ef73972b46f0f197a
Aldoar <aldo.castagnoli@gmail.com>
2022-04-20 17:34:43 -0400

    Agregar archivos de proyecto.

---
+++ Hola Mundo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    class Program
+    {
+        /// <summary>
+        /// Metodo que inicia el programa
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Permite escribir una linea en la consola
+            Console.WriteLine("Hola Mundo");
+            //Permite Pausar el programa hasta que presione una tecla
+            Console.ReadKey();
+        }
+    }
+}
